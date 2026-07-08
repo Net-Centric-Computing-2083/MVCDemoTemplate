f9344a2e7b3998e0d098fb2574268636a0008ff8
Nabin Kandel <nkandel208@gmail.com>
2026-07-08 10:18:06 -0700

    Add project files.

---
+++ MVCDemoTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDemoTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
