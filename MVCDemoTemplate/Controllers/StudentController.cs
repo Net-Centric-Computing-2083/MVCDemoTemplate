28e89947380f1939da4c4250c66c893a215f45c9
Nabin Kandel <nkandel208@gmail.com>
2026-07-15 19:42:25 -0700

    Update Model, controller and View

---
+++ MVCDemoTemplate/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemoTemplate.Models;
+
+namespace MVCDemoTemplate.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student
+            {
+                Id = 100,
+                Name = "Dinesh",
+                Faculty = "Computer Science and Information Technology",
+                Batch = "2083"
+            };
+            return View(student);
+        }
+    }
+}
---
+++ MVCDemoTemplate/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCDemoTemplate.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Faculty { get; set; }
+        public string? Batch { get; set; }
+    }
+}
